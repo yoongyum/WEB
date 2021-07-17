437de7412daf2f94f49b44bd8293395b9e2e9e1d
yoongyum <imshreg@naver.com>
2021-07-17 15:48:00 +0900

    initalize project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import EyeOn from './images/EyeOn.png'
+import './App.css';
+import TopBar from './components/TopBar';
+ 
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <header className="App-header">
+        <h1 className="App-title">SAVER</h1>
+        <img src={EyeOn} className="App-logo" alt="logo" />
+        <p>
+          리액트 프로젝트
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          리액트 배우러가기
+        </a>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import './TopBar.css';
+import Camera from '../images/Camera.png'
+function TopBar() {
+        return(
+                <div class="container">
+                        <p>Bar</p>
+                        <a type="button" class='Topbtn'/>
+                                
+                        
+                </div>
+        )
+}
+
+export default TopBar;
