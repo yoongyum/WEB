83c31cb843c5c280f38f2e539f786610f13aea6e
LimZida <zida4470@naver.com>
2021-07-21 17:22:48 +0900

    good

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import EyeOn from './images/EyeOn.png'
+
 import './App.css';
 import TopBar from './components/TopBar'
+import Grid from './components/Grid'
  
 function App() {
   return (
     <div className="App">
       <TopBar />
       <header className="App-header">
-        <h1 className="App-title">SAVER</h1>
-        <img src={EyeOn} className="App-logo" alt="logo" />
-        <p>
-          리액트 프로젝트 버전1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트 배우러가기
-        </a>
+        <Grid />
+        
       </header>
     </div>
-    
   );
 }
 
