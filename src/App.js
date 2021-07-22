198d20b70dd81e055151393a830a8db0b768bbd9
LimZida <zida4470@naver.com>
2021-07-22 17:10:49 +0900

    수고

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import TopBar from './components/TopBar'
 import Grid from './components/Grid'
+import Grid2 from './components/Grid2'
  
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <TopBar />
       <header className="App-header">
         <Grid />
+        <Grid2/>
       </header>
         
      
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,29 +3,56 @@ import Img1 from '../images/1.png'
 import Img2 from '../images/2.png'
 import Img3 from '../images/3.png'
 import Img4 from '../images/4.png'
+import { Fragment } from 'react'
 
 const Grid=()=>{
+    // const grid=document.querySelector(`.grid`)
+    // const btn=document.querySelector(`.btn`)
+    // function onClick(e){
+    //     if(e.target.classList.contains(`btn`)){
+    //     const text=document.createElement(`div`)
+    //     text.innerText="250mm~290mm"
+    //     btn.before(text)
+    //     }
+    // }
     function mouseOver(e){
         if(e.target.classList.contains(`grid`)){
-            return e.target.classList.add(`change`)
-        }
+           e.target.classList.add('grid:hover',`change_1`)
+        }  
         else{
-            return e.target.classList.toggle(`btn`,'btn:hover')
+           e.target.classList.add('btn:hover','change2')
         }
     } 
     function mouseOut(e){
         if(e.target.classList.contains(`grid`)){
-            return e.target.classList.remove(`change`)
+            e.target.classList.remove(`change_1`,'grid:hover')
         }
+        else{
+            e.target.classList.remove('change2','btn:hover')
+         }
     }
 
     return(
-        <>
-        <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid"><img src={Img1} width="200px" alt=""></img></div>
-        <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid"><img src={Img2} width="200px" alt=""></img></div>
-        <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid"><img src={Img3} width="200px" alt=""></img></div>
-        <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid"><img src={Img4} width="200px" alt=""></img></div>
-        </>
+        <Fragment>
+        <div className="all">
+            <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid">
+                <img src={Img1} className="btn" width="200px" alt=""></img>
+                <li>조던 신발1</li>
+                <li>가격:129,000,000</li></div>
+            <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid">
+                <img src={Img2} className="btn" width="200px" alt=""></img>
+                <li>조던 신발2</li>
+                <li>가격:19,000,000</li></div>
+            <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid">
+                <img src={Img3} className="btn" width="200px" alt=""></img>
+                <li>조던 신발3</li>
+                <li>가격:18,000,000</li></div>
+            <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid">
+                <img src={Img4} className="btn" width="200px" alt=""></img>
+                <li>조던 신발4</li>
+                <li>가격:1,129,000,000</li></div>
+        </div>
+        </Fragment>
     )
 }
 
