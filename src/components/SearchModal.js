ea13586d415c26c87edebc9963186a679c40d2f6
yoongyum <imshreg@naver.com>
2021-07-25 12:08:54 +0900

    검색창 설정

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -1,11 +1,17 @@
 import './SearchModal.css';
+import { FiSearch } from "react-icons/fi";
 
+const text = "브랜드명, 모델명 ,모델번호 등 "
 
 const SearchModal=({onModal})=>{
         return(
                 <div className="SContainer">
                         <div className='SearchArea'>
-                                <textarea type="text" className="search" placeholder="브랜드 명"/>
+                                <div className ="SearchBar">
+                                        <FiSearch style={dot} />
+                                        <textarea className="search" placeholder={text}/>
+                                </div>
+                                <p>취소</p>
                         </div>
                         <div className="BlackArea" onClick={onModal}/>
                 </div>
@@ -13,3 +19,9 @@ const SearchModal=({onModal})=>{
 }
 
 export default SearchModal;
+
+
+const dot = {
+        fontSize : 44,
+        color : "#959595"
+}
