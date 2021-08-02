681459c6db5c59e3892996e6f938c32dcb9fc5f2
LimZida <zida4470@naver.com>
2021-08-02 18:18:47 +0900

    챗박스 그리드 수정

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -11,12 +11,12 @@ import Img8 from '../images/8.png'
 import Img9 from '../images/화살표.png'
 // import Grid2 from './Grid2'
 
-let list=[{img:Img1,title:'조던 신발1',content:'가격:129,000,000',}
+let list=[{img:Img1,title:'조던 신발1',content:'가격:129,000,000'}
 ,{img:Img2,title:'조던 신발2',content:'가격:129,000,000',}
 ,{img:Img3,title:'조던 신발3',content:'가격:129,000,000',}
 ,{img:Img4,title:'조던 신발4',content:'가격:129,000,000',},
 ]
-let list2=[{img:Img5,title:'조던 신발1',content:'가격:129,000,000',}
+let list2=[{img:Img5,title:'조던 신발1',content:'가격:129,000,000'}
 ,{img:Img6,title:'조던 신발2',content:'가격:129,000,000',}
 ,{img:Img7,title:'조던 신발3',content:'가격:129,000,000',}
 ,{img:Img8,title:'조던 신발4',content:'가격:129,000,000',},
@@ -28,50 +28,47 @@ const Grid=()=>{
         SetModal(!modal);
     }
 
-    function mouseOver(e){
-        if(e.target.classList.contains(`grid`)){
-           e.target.classList.add('grid:hover',`change_1`)
-        }  
-        else{
-           e.target.classList.add('btn:hover','change2')
-        }
-    } 
-    function mouseOut(e){
-        if(e.target.classList.contains(`grid`)){
-            e.target.classList.remove(`change_1`,'grid:hover')
-        }
-        else{
-            e.target.classList.remove('change2','btn:hover')
-         }
-    }
 
-    function mouseOver1(e){
-        if(e.target.classList.contains(`grid`)){
-           e.target.classList.add('grid:hover',`change_2`)
-        }  
-        else{
-           e.target.classList.add('btn:hover','change2')
+    function mouseDown(e){
+        if(e.target.classList.contains(`fake`)&&e.button===0){
+            e.target.classList.add('grid_hover','change_1')
+            e.target.classList.remove('fake')
+            e.target.firstElementChild.classList.remove(`stopEvent`)
+        }
+        else if((!e.target.classList.contains(`fake`)&&e.target.classList.contains(`grid`))&&e.button===0) {
+             e.target.classList.remove('grid_hover','change_1')
+             e.target.classList.add(`fake`)
+             e.target.firstElementChild.classList.add(`stopEvent`)
         }
     } 
-    function mouseOut1(e){
-        if(e.target.classList.contains(`grid`)){
-            e.target.classList.remove(`change_2`,'grid:hover')
+
+    function mouseDown2(e){
+        if(e.target.classList.contains(`grid`)&&e.button===0){
+            e.target.classList.add('grid_hover','change_2')
+            e.target.firstElementChild.classList.remove(`stopEvent`)
         }
-        else{
-            e.target.classList.remove('change2','btn:hover')
-         }
-    }
-        
+        if((e.target.classList.contains(`grid`))&&e.button===2) {
+             e.target.classList.remove('grid_hover','change_2')
+             e.target.firstElementChild.classList.add(`stopEvent`)
+        }
+    } 
+
+    function del(e){
+            e.preventDefault();
+      }
     
+
     return(
-        <div className="mother">
+        <div  onContextMenu={del} className="mother">
             <div className="all">
                 {
                     list.map((val)=>{
-                        return <div onMouseEnter={mouseOver} onMouseLeave={mouseOut} className="grid">
-                        <img src={val.img} className="imgset" width="200px" alt=""></img>
-                        <li>{val.title}</li>
-                        <li>{val.content}</li></div>
+                        return <div onMouseDown={mouseDown} onContextMenu={del} className="grid fake">
+                        <img src={val.img} className="imgset stopEvent" width="200px" alt=""></img>
+                        <li className="list">{val.title}</li>
+                        <li>{val.content}</li>
+                        </div>
+
                     })
                 }
             </div>
@@ -79,10 +76,12 @@ const Grid=()=>{
             <div className="all">
                     {modal?
                         list2.map((val)=>{    
-                        return <div onMouseEnter={mouseOver1} onMouseLeave={mouseOut1} className="grid">
-                        <img src={val.img} className="imgset" width="200px" alt=""></img>
+                        return <div onMouseDown={mouseDown2} onContextMenu={del} className="grid fake">
+                        <img src={val.img} className="imgset stopEvent" width="200px" alt=""></img>
                         <li>{val.title}</li>
-                        <li>{val.content}</li></div>}):null} 
+                        <li>{val.content}</li>
+                        </div>
+                    }):null} 
             </div>
             <button className="Btn" onClick={ClickModal}><img src={Img9} width="75px" alt=""></img></button>
         </div>
